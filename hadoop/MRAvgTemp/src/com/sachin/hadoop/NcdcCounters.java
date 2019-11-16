78dcb88faa289187f4b240829e0d92d8a03219dc
Sachin Sunbeam workshop <sachinbhadrashette@gmail.com>
2019-11-16 13:59:54 +0530

    Added concept of counters

---
+++ hadoop/MRAvgTemp/src/com/sachin/hadoop/NcdcCounters.java
@@ -0,0 +1,4 @@
+package com.sachin.hadoop;
+public enum NcdcCounters {
+      InvalidRecords, InvalidReadings
+  }
--- hadoop/MRAvgTemp/src/com/sachin/hadoop/TempMapper.java
+++ hadoop/MRAvgTemp/src/com/sachin/hadoop/TempMapper.java
@@ -8,19 +8,28 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class TempMapper extends Mapper<LongWritable,Text, IntWritable, DoubleWritable> {
       @Override
       public void map(LongWritable key, Text value, Mapper<LongWritable,Text, IntWritable,DoubleWritable>.Context ctx) throws IOException, InterruptedException {
           String line = value.toString();
-          
-          List<String> criteria = Arrays.asList("0", "1", "4", "5", "9");
-          String quality = line.substring(92, 93);
-          if(criteria.contains(quality)) {
-        	  IntWritable year =new IntWritable(Integer.parseInt(line.substring(15, 19)));
-        	  DoubleWritable temp = new DoubleWritable(Double.parseDouble(line.substring(87, 92)));
-              ctx.write(year, temp);
+          try {
+	          List<String> criteria = Arrays.asList("0", "1", "4", "5", "9");
+	          String quality = line.substring(92, 93);
+	          if(criteria.contains(quality)) {
+	        	  IntWritable year =new IntWritable(Integer.parseInt(line.substring(15, 19)));
+	        	  DoubleWritable temp = new DoubleWritable(Double.parseDouble(line.substring(87, 92)));
+	              ctx.write(year, temp);
+	          } else {
+	        	  Counter cntr = ctx.getCounter(NcdcCounters.InvalidReadings);
+	        	  cntr.increment(1);
+	          }
+          } catch(Exception ex) {
+        	  Counter cntr = ctx.getCounter(NcdcCounters.InvalidRecords);
+        	  cntr.increment(1);
+        	  ex.printStackTrace();
           }
       }
   }
